4b51f179d2cfa795bef0f7d099292fffc5c27ac6
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2023-08-29 17:11:12 -0300

    Order, Instant, ISO 8601 - part 2

---
+++ src/main/java/com/luisfelipebp/course/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.luisfelipebp.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.luisfelipebp.course.entities.Order;
+import com.luisfelipebp.course.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository repository;
+	
+	public List<Order> findAll(){
+		return repository.findAll();
+	}
+
+	public Order findById(Long id) {
+		Optional<Order> obj = repository.findById(id);
+		return obj.get();
+	}
+}
