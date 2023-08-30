3d79fa8fc06a8eb5cbe3f3672f6c3c9fe6507bd4
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2023-08-30 13:52:11 -0300

    Product' git commit -m Product
    cls
    clear
    '

---
+++ src/main/java/com/luisfelipebp/course/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.luisfelipebp.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luisfelipebp.course.entities.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
+
