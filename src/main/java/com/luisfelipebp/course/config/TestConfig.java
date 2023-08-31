846d099c8853fb8265a49b6a346ecd2ccc331294
luisfelipebp <luisfelipebarbosa2013@gmail.com>
2023-08-31 15:01:22 -0300

    OneToOne association

--- src/main/java/com/luisfelipebp/course/config/TestConfig.java
+++ src/main/java/com/luisfelipebp/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.luisfelipebp.course.entities.Category;
 import com.luisfelipebp.course.entities.Order;
 import com.luisfelipebp.course.entities.OrderItem;
+import com.luisfelipebp.course.entities.Payment;
 import com.luisfelipebp.course.entities.Product;
 import com.luisfelipebp.course.entities.User;
 import com.luisfelipebp.course.entities.enums.OrderStatus;
@@ -81,6 +82,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 
 	
